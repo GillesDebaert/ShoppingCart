3fd253e42bca1a74fe025ffdcb199913ae8acec0
gideb <gideb@PC-GILLES>
2025-12-18 16:18:59 +0100

    Add project files.

---
+++ Data/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingCart.Data
+{
+	public class Company
+	{
+		public required int Id { get; set; }
+		public required string BusinessName { get; set; }
+		public string? VAT { get; set; }
+		public required string RegistrationNumber { get; set; }
+		public required decimal Turnover { get; set; }
+	}
+}
---
+++ Data/PrivateIndividual.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Data
+{
+	public class PrivateIndividual
+	{
+		public required int Id { get; set; }
+		public required string FirstName { get; set; }
+		public required string LastName { get; set; }
+	}
+}
